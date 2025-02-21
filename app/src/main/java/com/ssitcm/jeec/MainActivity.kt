8b868591eb4e2a23855d68e880c91e0b2c2d85dc
Rodrigo Amador <rodrigoalbertoamador@gmail.com>
2025-02-21 17:17:41 -0600

    Creación del proyecto.

---
+++ app/src/main/java/com/ssitcm/jeec/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.ssitcm.jeec
+
+/*   Diseñado y programado por Rodrigo Alberto Amador García y Erick Jhoan Hernández Delgado  */
+/*   "Juntas en el ciclo" es una aplicación para brindar información sobre la menstruación.   */
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ssitcm.jeec.ui.theme.JEECTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JEECTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JEECTheme {
+        Greeting("Android")
+    }
+}
